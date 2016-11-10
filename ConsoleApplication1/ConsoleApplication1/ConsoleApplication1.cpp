36bf12992ed781eef3aa87d0aa8b85e53110d4a9
Nelson Victor Cruz Hernandez <victor.cruz.isc@gmail.com>
2016-11-10 09:02:48 -0600

    Add Chapter 2 review
    This commit includes the review exercise and the thrill included in the
    book.
    
    The problem is simple and just mofies the hello worl basic example.

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+inline void keep_window_open() { char ch; cin >> ch; }
+
+int main()
+{
+	cout << "Hello programming!" << endl;
+	cout << "Here we go!" << endl;
+	keep_window_open();
+    return 0;
+}
